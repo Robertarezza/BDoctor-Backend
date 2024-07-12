2b29d8d87f0da8db92a20cd87dc7463710d8f8f9
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-07-12 15:00:27 +0200

    add: migrations

---
+++ database/migrations/2024_07_12_104507_create_rewiews_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rewiews', function (Blueprint $table) {
+            $table->unsignedBigInteger('doctor_id')->index();
+            $table->id()->foreign('doctor_id')->references('id')->on('doctors')->cascadeOnDelete();
+            $table->string('guest_name');
+            $table->string('guest_mail')->nullable();
+            $table->text('rewiew');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rewiews');
+    }
+};
