47c444891e02d5a7290ef5a7bd19094333bd3c77
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-07-12 15:50:32 +0200

    add: relationship  from user to doctor

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function doctor() {
+        return $this->hasOne(Doctor::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
@@ -19,6 +23,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'password',
     ];
