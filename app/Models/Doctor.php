e06ce8803fbd6921d0c795a35483e754aa0f236f
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-07-13 11:42:29 +0200

    add: specialization model and seeder

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -14,4 +14,8 @@ class Doctor extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function specializations() {
+        return $this->belongsToMany(Specialization::class);
+    }
 }
