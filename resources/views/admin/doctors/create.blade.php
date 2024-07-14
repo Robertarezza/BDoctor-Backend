54a73e03c041ab6d656364df6376c8dcb13dec01
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-07-15 00:06:10 +0200

    feat: added validation message under all inputs

--- resources/views/admin/doctors/create.blade.php
+++ resources/views/admin/doctors/create.blade.php
@@ -2,51 +2,86 @@
 
 @section('content')
     <h1 class="text-center m-5">Crea il tuo profilo</h1>
+    
+    <div class="container">
+        <form action="{{ route('admin.doctors.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
 
-    @include('partials.errors')
-    <form action="{{ route('admin.doctors.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-
-        <div class="mb-3">
-            <label for="photo" class="form-label">Inserisci una foto *</label>
-            <input class="form-control" name="photo" type="file" id="photo">
-        </div>
-        <div class="mb-3">
-            <img class="d-none doctor-photo" id="photo-preview" src="" alt="">
-        </div>
-        <div class="mb-3">
-            <label for="specialization" class="form-label">Specializzazioni *</label>
-            <select name="specialization[]" id="specialization" class="form-control d-none" multiple>
-                @foreach ($specializations as $specialization)
-                    <option value="{{ $specialization->id }}">{{ $specialization->title }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="performance" class="form-label">Prestazioni *</label>
-            <select name="performance[]" id="performance" class="form-control d-none" multiple>
-                @foreach ($performances as $performance)
-                    <option value="{{ $performance->id }}">{{ $performance->title }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="phone_number" class="form-label">Numero di telefono *</label>
-            <input type="number" name="phone_number" class="form-control" id="phone_number">
-        </div>
-        <div class="mb-3">
-            <label for="studio_address" class="form-label">Indirizzo studio *</label>
-            <input type="text" name="studio_address" class="form-control" id="studio_address">
-        </div>
-        <div class="mb-3">
-            <label for="CV" class="form-label">Allega il tuo CV *</label>
-            <input class="form-control" name="CV" type="file" id="CV">
-        </div>
-        <div class="mb-3">
-            <img class="d-none doctor-cv" id="cv-preview" src="" alt="">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+            <div class="mb-3">
+                <label for="photo" class="form-label">Inserisci una foto *</label>
+                <input name="photo" type="file" id="photo" class="form-control @error('photo') is-invalid @enderror">
+                @error('photo')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <img class="d-none doctor-photo" id="photo-preview" src="" alt="">
+            </div>
+            <div class="mb-3">
+                <label for="specialization" class="form-label">Specializzazioni *</label>
+                <select name="specialization[]" id="specialization"
+                    class="form-control @error('specialization') is-invalid @enderror d-none" multiple>
+                    @foreach ($specializations as $specialization)
+                        <option value="{{ $specialization->id }}">{{ $specialization->title }}</option>
+                    @endforeach
+                </select>
+                @error('specialization')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="performance" class="form-label">Prestazioni *</label>
+                <select name="performance[]" id="performance"
+                    class="form-control @error('performance') is-invalid @enderror d-none" multiple>
+                    @foreach ($performances as $performance)
+                        <option value="{{ $performance->id }}">{{ $performance->title }}</option>
+                    @endforeach
+                </select>
+                @error('performance')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="phone_number" class="form-label">Numero di telefono *</label>
+                <input type="number" name="phone_number" id="phone_number"
+                    class="form-control @error('phone_number') is-invalid @enderror">
+                @error('phone_number')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="studio_address" class="form-label">Indirizzo studio *</label>
+                <input type="text" name="studio_address" id="studio_address"
+                    class="form-control @error('studio_address') is-invalid @enderror">
+                @error('studio_address')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="CV" class="form-label">Allega il tuo CV *</label>
+                <input name="CV" type="file" id="CV" class="form-control @error('CV') is-invalid @enderror">
+                @error('CV')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <img class="d-none doctor-cv" id="cv-preview" src="" alt="">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
     <script>
         // https://github.com/habibmhamadi/multi-select-tag
         new MultiSelectTag('specialization', {
@@ -75,6 +110,5 @@
                 console.log(values)
             }
         })
-
     </script>
 @endsection
