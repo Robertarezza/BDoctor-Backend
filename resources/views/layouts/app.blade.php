61662c69b69c02803ac32d5f33d87b746371013b
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-07-15 16:31:49 +0200

    fix: styles in dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,58 +23,56 @@
     <div id="app">
 
 
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm ms-header">
             <div class="container">
                 <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
                     <div class="logo_laravel">
                         <img src="{{ asset('img/logo.png') }}" alt="Logo Personalizzato" style="width: 62px;">
-                        <span style="font-size: smaller; font-weight: 900;">D-Doctor</span>
+                        <span style="font-size: smaller; font-weight: 900;">B-Doctor</span>
                     </div>
                     {{-- config('app.name', 'Laravel') --}}
                 </a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" style="font-size: smaller; font-weight: 900;" href="{{ url('/') }}">{{ __('Home') }}</a>
-                        </li>
-                    </ul>
-
+                <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
                         @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item"
+                                        href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
                         @endguest
                     </ul>
                 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,43 @@
 @extends('layouts.app')
 @section('content')
+    @if (session('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif
 
-@if (session('message'))
-<div class="alert alert-success">
-    {{ session('message') }}
-</div>
-@endif
-
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
-    <div class="container py-5">
-        <div class="d-flex justify-content-center gap-3">
-            <div class="logo_laravel">
-                <img src="{{ asset('img/logo.png') }}" alt="Logo Personalizzato" style="width: 62px;">
-                <span style="font-size: smaller; font-weight: 900;">D-Doctor</span>
-            </div>
-            <h1 class="display-5 fw-bold">
-                Benvenuto nella Dashbord di D-Doctor
+    <div class="jumbotron p-5 bg-light ms-jumbotron">
+        <div class="container py-5">
+            <h1 class="display-5 fw-bold text-center text-white">
+                Benvenuto nella Dashboard di B-Doctor
             </h1>
-        </div>
-        <p class=" text-center" style="font-size: 45px; font-weight: 900;">Scegli una delle opzioni</p>
-        <div class="d-flex justify-content-center gap-5">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body text-center">
-                <h5 class="card-title">Login</h5>
-                <p class="card-text">Effettua il Login alla Dshboard per gestire al meglio il tuo profilo e visualizza messaggi e recensioni</p>
-                <a  href="{{ route('login') }}" class="btn btn-primary">{{ __('Login') }}</a>
+            <p class=" text-center text-white" style="font-size: 25px; font-weight: 900;">Scegli una delle opzioni</p>
+            <div class="d-flex justify-content-center gap-5 mt-5">
+                <div class="card rounded-4 ms-card mt-5" style="width: 18rem;">
+                    <div class="card-body text-center d-flex flex-column align-items-center justify-content-between">
+                        {{-- <h5 class="card-title">Login</h5> --}}
+                        <p class="card-text">Effettua il Login alla Dashboard per gestire al meglio il tuo profilo e
+                            visualizzare messaggi e recensioni</p>
+                        <a href="{{ route('login') }}" class="btn btn-primary w-75">{{ __('Login') }}</a>
+                    </div>
+                </div>
+                <div class="card rounded-4 ms-card mt-5" style="width: 18rem;">
+                    <div class="card-body text-center d-flex flex-column align-items-center justify-content-between">
+                        {{-- <h5 class="card-title">Registrati</h5> --}}
+                        <p class="card-text">Registrati gratuitamente alla Dashboard per gestire al meglio il tuo profilo e
+                            visualizzare messaggi e recensioni</p>
+                        <a href="{{ route('register') }}" class="btn btn-primary w-75">{{ __('Register') }}</a>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-            <div class="card-body text-center">
-                <h5 class="card-title">Registrati</h5>
-                <p class="card-text">Registrati gratuitamente alla Dshboard per gestire al meglio il tuo profilo e visualizza messaggi e recensioni</p>
-                <a href="{{ route('register') }}" class="btn btn-primary">{{ __('Register') }}</a>
-            </div>
-        </div>
         </div>
     </div>
-</div>
 
-<div class="content">
-    <div class="container">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
-    </div>
-</div>
+    {{-- <div class="content">
+        <div class="container">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi
+                deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis
+                accusamus dolores!</p>
+        </div>
+    </div> --}}
 @endsection
