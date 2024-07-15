6e7b358b4023665887da37b5924b71cd856c7fe9
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-07-15 17:33:56 +0200

    add: sayhello for the user in homepage

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8 mt-4">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-header">Homepage</div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -14,7 +14,7 @@
                             </div>
                         @endif
 
-                        {{ __('You are logged in!') }}
+                        <h1>Bentornato! {{ $user->name }}</h1>
                     </div>
                 </div>
             </div>
