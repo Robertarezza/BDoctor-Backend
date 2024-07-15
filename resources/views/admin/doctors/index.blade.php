53afc6498ff2d3f1984f7e6201c1534be9ebd772
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-07-15 10:25:40 +0200

    fix: fixed displayer in index page

--- resources/views/admin/doctors/index.blade.php
+++ resources/views/admin/doctors/index.blade.php
@@ -84,7 +84,7 @@
                 <tr>
                     <th>CV</th>
                     <td>
-                        <img class="doctor-cv" src=" {{ asset('storage/' . $doctor->CV) }}" alt="">
+                        <embed class="doctor-cv" src=" {{ asset('storage/' . $doctor->CV) }}" alt="">
                     </td>
                 </tr>
                 {{-- /CV --}}
