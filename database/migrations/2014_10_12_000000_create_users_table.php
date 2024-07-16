5579daf2cd57a8bb6c769ee65cbff29569c31d4a
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-07-17 00:39:32 +0200

    feat: modified unique validation to mail for softdelete

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('surname');
-            $table->string('email')->unique();
+            $table->string('email'); //->unique(); tolto per evitare un conflitto con la solftdelete
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
