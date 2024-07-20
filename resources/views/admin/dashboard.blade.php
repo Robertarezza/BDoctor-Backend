071a3cf1d41067d66d41c3a59343f0a2f99c1cb1
francesco-mauro <francesco.mauro180@gmail.com>
2024-07-20 17:04:46 +0200

    add: Homepage fix

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,34 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center t">
+    <div class="row justify-content-center">
         <div class="col-md-8 mt-4">
-            <div class="card bg-none">
-                {{-- <div class="card-header">Homepage</div> --}}
-
-                <div class="card-body ">
+            <div class="card bg-light shadow-sm">
+                <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
-
                     <h1 class="text-capitalize text-center">Benvenuto! Dr. {{ $user->name }} {{ $user->surname }}</h1>
                 </div>
-
             </div>
         </div>
     </div>
 </div>
-@if ($reviews) {{-- Controlla se la variabile $reviews è settata --}}
+@if ($reviews)
 <div class="container mt-4">
-    <h2 class="text-center mb-4 border text-light">La tua ultima recensione</h2>
+    <h2 class="text-center mb-4 text-light">La tua ultima recensione</h2>
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
+        <div class="col-md-8">
+            <div class="card shadow-sm">
                 <div class="card-body">
-                    <h1 class="card-title">{{ ucfirst(strtolower($reviews->guest_name)) }}</h1>
+                    <h3 class="card-title">{{ ucfirst(strtolower($reviews->guest_name)) }}</h3>
                     <p class="card-text">{{ $reviews->review }}</p>
+                    <hr>
                     <p><strong>Data:</strong> {{ $reviews->created_at->format('d-m-Y H:i') }}</p>
                     <p><strong>Email:</strong> {{ $reviews->guest_mail }}</p>
                 </div>
@@ -39,8 +36,7 @@
 </div>
 @else
 <div class="container mt-4">
-    <h2 class="text-center mb-4 border text-light">Nessuna recensione presente</h2>
+    <h2 class="text-center mb-4 text-light">Nessuna recensione presente</h2>
 </div>
 @endif
-
 @endsection
