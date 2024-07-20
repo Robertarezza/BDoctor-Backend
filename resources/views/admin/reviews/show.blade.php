fa651ca8a118d0795e181b2cdf25e436eb0bb2a0
francesco-mauro <francesco.mauro180@gmail.com>
2024-07-20 16:54:06 +0200

    feat: new review detail layout

--- resources/views/admin/reviews/show.blade.php
+++ resources/views/admin/reviews/show.blade.php
@@ -3,13 +3,14 @@
 @section('content')
 <div class="container mt-5">
     <h1 class="text-center text-light">Dettaglio della recensione</h1>
-    <h5 class="text-light mt-3">{{ ucfirst(strtolower($review->guest_name)) }} scrive :</h5>
-       <div class="card" style="width: 18rem;">
+    <h5 class="text-light mt-3">{{ ucfirst(strtolower($review->guest_name)) }} scrive:</h5>
+    <div class="card mt-3 shadow-sm" style="width: 100%;">
         <div class="card-body">
+            <h5 class="card-title">Recensione</h5>
             <p class="card-text">{{$review->review}}</p>
-            <p>Il {{$review->created_at}}</p>
-            <p>Indirizzo mail {{$review->guest_mail}}</p>
-            
+            <hr>
+            <p class="text-muted">Il <strong>{{ $review->created_at->format('d-m-Y H:i') }}</strong></p>
+            <p class="text-muted">Indirizzo mail: <strong>{{ $review->guest_mail }}</strong></p>
         </div>
     </div>
 </div>
