f3fe21239ceb05bbdf37f9903c07637c730b2fab
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-07-20 15:47:22 +0200

    add: carbon date

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -4,20 +4,20 @@
     <div class="container p-5">
         @if ($messages)
             <div class="row">
-                <table class="table">
+                <table class="table table-borderless bg-transparent">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">name</th>
-                            <th scope="col">surname</th>
-                            <th scope="col">e-mail</th>
-                            <th scope="col">message</th>
+                            <th scope="col">Data</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Cognome</th>
+                            <th scope="col">E-mail</th>
+                            <th scope="col">Messaggio</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($messages as $message)                            
                             <tr>
-                                <th scope="row">{{ $loop->iteration }}</th>
+                                <th scope="row">{{ \Carbon\Carbon::parse($message->created_at)->format('d-m-Y H:i') }}</th>
                                 <td>{{ $message->guest_name }}</td>
                                 <td>{{ $message->guest_surname }}</td>
                                 <td>{{ $message->guest_mail }}</td>
