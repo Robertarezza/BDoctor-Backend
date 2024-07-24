f41a4b0349a87667ed07f1a847416dfd0a30d8e7
Robertarezza <roberta.rezza@gmail.com>
2024-07-24 19:13:53 +0200

    fix

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
 
     // braintree service with the key's we saved in .env
     'braintree' => [
-        'environment' => env('BRAINTREE_ENVIRONMENT'),
+        'environment' => env('BRAINTREE_ENVIRONMENT', 'sandbox'),
         'merchant_id' => env('BRAINTREE_MERCHANT_ID'),
         'public_key' => env('BRAINTREE_PUBLIC_KEY'),
         'private_key' => env('BRAINTREE_PRIVATE_KEY'),
