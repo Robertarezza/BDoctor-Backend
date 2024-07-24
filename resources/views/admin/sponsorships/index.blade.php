e54d376cccb2fa05beaf6eb52cb679201e93a4c0
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-07-24 16:16:09 +0200

    add: style on sponsorhip cards

--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -14,19 +14,18 @@
 
         <div class="row">
             @foreach ($sponsorships as $sponsorship)
-            <div class="col-md-4">
-                <div class="card mb-4">
+            <div class="col-md-4" id="card-container">
+                <div class="card mb-4 p-5" id="{{ $sponsorship->name }}">
                     <div class="card-body">
                         <h5 class="card-title text-center border-bottom pb-2">{{ $sponsorship->name }}</h5>
                         <p class="card-text text-center mt-3"><strong>Prezzo:</strong> {{ number_format($sponsorship->price, 2) }} €</p>
                         <p class="card-text text-center"><strong>Durata:</strong> {{ $sponsorship->duration }} ore</p>
                     </div>
-                    <form action="{{ route('admin.sponsorships.store') }}" method="POST">
+                    <form action="{{ route('admin.sponsorships.store') }}" method="POST" class="d-flex justify-content-center mt-3">
                         @csrf
                         <input type="hidden" name="sponsorship_id" value="{{ $sponsorship->id }}">
-                        <button type="submit" class="btn btn-primary">Attiva Sponsorizzazione</button>
+                        <button type="submit" class="btn btn-primary" id="SponsorBtn">Attiva Sponsorizzazione</button>
                     </form>
-
                 </div>
             </div>
             @endforeach
